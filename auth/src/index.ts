f46fc7772beffc3ecd8d07740ed6a3832aa65d68
Emmanuel Ndukwe <emasysnd@gmail.com>
2020-11-09 11:26:12 +0100

    config k8s cluster routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,10 @@ const PORT = 3000;
 const app = express();
 app.use(json());
 
+app.get('/api/users/currentuser', (req, res) => {
+    res.send('hi there')
+})
+
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}!`);
 });
