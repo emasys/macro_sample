1294b41c2325ba67d52f96ff6ebe026d53934929
Emmanuel Ndukwe <emasysnd@gmail.com>
2020-11-16 11:10:36 +0100

    add current user route

---
+++ auth/src/errors/authentication-error.ts
@@ -0,0 +1,14 @@
+import { CustomError } from './custom-error';
+
+export class AuthenticationError extends CustomError {
+  statusCode = 401;
+
+  constructor(public message: string) {
+    super(message);
+    Object.setPrototypeOf(this, AuthenticationError.prototype);
+  }
+
+  serializeErrors() {
+    return [{ message: this.message }];
+  }
+}
--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import { AuthenticationError } from '../errors/authentication-error';
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-  res.send('hi there!');
+  if (!req.session?.jwt) {
+    throw new AuthenticationError('user not authorized');
+  }
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    return res.send({ currentUser: payload });
+  } catch (error) {
+    throw new AuthenticationError('user not authorized');
+  }
 });
 
 export { router as currentUserRouter };
