5399e096379cc46190a5274406d32c43906b45f9
Emmanuel Ndukwe <emasysnd@gmail.com>
2020-11-16 11:39:48 +0100

    add auth middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,19 +1,15 @@
-import express from 'express';
-import jwt from 'jsonwebtoken';
-import { AuthenticationError } from '../errors/authentication-error';
+import express, { Request, Response } from 'express';
+import { currentUser } from '../middlewares/current-user';
+import { requireAuth } from '../middlewares/require-auth';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-  if (!req.session?.jwt) {
-    throw new AuthenticationError('user not authorized');
+router.get(
+  '/api/users/currentuser',
+  [currentUser, requireAuth],
+  (req: Request, res: Response) => {
+    return res.send({ currentUser: req.currentUser || null });
   }
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    return res.send({ currentUser: payload });
-  } catch (error) {
-    throw new AuthenticationError('user not authorized');
-  }
-});
+);
 
 export { router as currentUserRouter };
